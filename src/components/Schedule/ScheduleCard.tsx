02a5389ef6d36b742554b6ba9e0cebc574ee6895
mwila <mpnyirongo@gmail.com>
2024-03-20 16:02:13 +0200

    feat: Created an animated progress bar for the schedule card

--- src/components/Schedule/ScheduleCard.tsx
+++ src/components/Schedule/ScheduleCard.tsx
@@ -1,8 +1,61 @@
-import { Badge, Card, Group, Stack, Text, useMantineTheme } from '@mantine/core';
-import { format, isWithinInterval } from 'date-fns';
+import { useEffect, useState } from 'react';
+import {
+  Badge,
+  Button,
+  Card,
+  Group,
+  Paper,
+  Progress,
+  Stack,
+  Text,
+  Transition,
+  useMantineTheme,
+} from '@mantine/core';
+import { useShallowEffect } from '@mantine/hooks';
+import { differenceInMinutes, format, isWithinInterval } from 'date-fns';
 import { ScheduleCardProps } from '@/types';
 import { formatDay, remainingTime, removeProvince, toTitleCase } from '@/utils';
 
+const ScheduleCardProgess = ({ value }: { value: number }) => {
+  const [progressValue, setProgressValue] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setProgressValue(value);
+    }, 100);
+    return () => {
+      setProgressValue(0);
+    };
+  }, []);
+
+  return (
+    <Transition
+      mounted={progressValue !== 0}
+      transition="scale-x"
+      duration={700}
+      timingFunction="ease"
+    >
+      {(transitionStyle) => (
+        <Progress
+          value={progressValue}
+          pos="absolute"
+          h={7}
+          bottom={0}
+          left={0}
+          right={0}
+          color="red"
+          style={{ ...transitionStyle, backgroundColor: 'inherit', zIndex: 1 }}
+        />
+      )}
+    </Transition>
+  );
+};
+
+const remainingTimePercent = (startDate: Date, endDate: Date) => {
+  const totalTimeDiff = differenceInMinutes(endDate, startDate);
+  const timeDiffToEnd = differenceInMinutes(endDate, new Date());
+  const timeDiffPercent = Number(((totalTimeDiff - timeDiffToEnd) / totalTimeDiff) * 100);
+  return timeDiffPercent;
+};
 const ScheduleCard = ({ data, province }: ScheduleCardProps) => {
   const theme = useMantineTheme();
   const currentDate = new Date();
@@ -15,6 +68,7 @@ const ScheduleCard = ({ data, province }: ScheduleCardProps) => {
   });
 
   const timeToGo = remainingTime(scheduleStartDate, currentDate);
+  const progressValue = remainingTimePercent(scheduleStartDate, scheduleEndDate);
 
   return (
     <Card shadow="sm" padding="lg" radius="md" mb={10}>
@@ -47,6 +101,7 @@ const ScheduleCard = ({ data, province }: ScheduleCardProps) => {
           {isCurrentlyShedding ? 'Load Shedding In Progress' : timeToGo.text}
         </Badge>
       </Stack>
+      {isCurrentlyShedding && <ScheduleCardProgess value={progressValue} />}
     </Card>
   );
 };
